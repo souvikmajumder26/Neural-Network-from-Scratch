a47bfd4ab06192280819290ba4734738e76c0ea1
Souvik Majumder <sm.souvikmajumder@gmail.com>
2023-02-12 05:03:04 +0530

    modified:   src/utils/network.py

--- src/utils/network.py
+++ src/utils/network.py
@@ -3,7 +3,7 @@ import numpy as np
 # the neural network architecture: one input layer, two hidden layers, and one output layer
 
 # initialize the weights and biases with random values
-def init_params(X):
+def init_params(X, units_layer_h1, units_layer_h2, units_layer_output):
 
     # n = number of pixels in each image
     n, _ = X.shape
@@ -21,21 +21,21 @@ def init_params(X):
     # the shape of weight matrix between the input layer and the first hidden layer:
     # (number of units/neurons in the layer, number of pixels in each image, i.e., number of rows in the input data matrix).
     # for the fashion-mnist dataset, each image is 28*28, therefore the flattened image size is 768 which was assigned to the variable 'n'
-    W1 = np.random.rand(10, n) - 0.5
+    W1 = np.random.rand(units_layer_h1, n) - 0.5
     # initialization of biases can be done either by rand or zeros:
     # > any of the options is feasible since the non-zero weights will not let the gradients vanish
 
     # the shape of bias matrix for the first hidden layer:
     # (number of units/neurons in the layer, 1).
-    b1 = np.random.rand(10, 1)
+    b1 = np.random.rand(units_layer_h1, 1)
     # weight matrix between the first and the second hidden layer
-    W2 = np.random.rand(20, 10) - 0.5
+    W2 = np.random.rand(units_layer_h2, units_layer_h1) - 0.5
     # bias matrix for the neurons in the second hidden layer
-    b2 = np.random.rand(20, 1)
+    b2 = np.random.rand(units_layer_h2, 1)
     # weight matrix between the second and the third hidden layer
-    W3 = np.random.rand(10, 20) - 0.5
+    W3 = np.random.rand(units_layer_output, units_layer_h2) - 0.5
     # bias matrix for the nerons in the third hidden layer
-    b3 = np.random.rand(10, 1)
+    b3 = np.random.rand(units_layer_output, 1)
     return W1, b1, W2, b2, W3, b3
 
 # define the activation function for the hidden layers
