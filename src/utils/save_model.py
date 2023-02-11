55bd5fb8142ac0fa3ee3a004504225817ed46bb4
Souvik Majumder <sm.souvikmajumder@gmail.com>
2023-02-12 05:03:16 +0530

    modified:   src/utils/save_model.py

--- src/utils/save_model.py
+++ src/utils/save_model.py
@@ -2,7 +2,7 @@ import pickle
 
 # save the trained model parameters: weights and biases
 def save_model_params(W1, b1, W2, b2, W3, b3, model_path):
-    params_file = open(model_path,"wb")
+    params_file = open(model_path, "wb")
     params = (W1, b1, W2, b2, W3, b3)
     pickle.dump(params, params_file)
     params_file.close()
