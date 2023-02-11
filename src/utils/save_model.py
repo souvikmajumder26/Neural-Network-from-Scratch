ec888a6f0e9519e8513dc9bfc2960a31f6472e21
Souvik Majumder <sm.souvikmajumder@gmail.com>
2023-02-12 04:01:23 +0530

    modified:   src/utils/save_model.py

--- src/utils/save_model.py
+++ src/utils/save_model.py
@@ -1,9 +1,8 @@
 import pickle
 
 # save the trained model parameters: weights and biases
-def save_model_params(W1, b1, W2, b2, W3, b3):
+def save_model_params(W1, b1, W2, b2, W3, b3, model_path):
+    params_file = open(model_path,"wb")
     params = (W1, b1, W2, b2, W3, b3)
-    params_file = open(r"C:\Users\smsou\OneDrive\MY-DEVICE\Sync\Documents\GitHub\Neural-Network-from-Scratch\model\model.pkl","wb")
     pickle.dump(params, params_file)
     params_file.close()
-    print("Model parameters saved!")
