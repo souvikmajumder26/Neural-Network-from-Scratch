c535bb5b276076f138bbf6fee37e56ca27b3a98a
Souvik Majumder <sm.souvikmajumder@gmail.com>
2023-02-11 23:54:30 +0530

    modified:   src/train.py 	new file:   src/utils/__pycache__/dataset.cpython-310.pyc 	renamed:    src/utils/save.py -> src/utils/save_model.py

--- src/train.py
+++ src/train.py
@@ -1,5 +1,5 @@
 from utils.dataset import *
-from utils.save import *
+from utils.save_model import *
 from utils.fit import *
 
 # load and preprocess the dataset
@@ -7,7 +7,7 @@ X_train, y_train, X_test, y_test = load_data()
 X_train, X_test = preprocess_data(X_train, y_train)
 
 # fit the defined neural network model with the training data and get the final weights and biases
-W1, b1, W2, b2, W3, b3 = gradient_descent(X_train, y_train, 0.10, 2000)
+W1, b1, W2, b2, W3, b3 = gradient_descent(X_train, y_train, 0.10, 5)
 
 # save the trained model parameters: weights and biases
-save_params(W1, b1, W2, b2, W3, b3)
+save_model_params(W1, b1, W2, b2, W3, b3)
--- src/utils/save_model.py
+++ src/utils/save_model.py
@@ -1,7 +1,7 @@
 import pickle
 
 # save the trained model parameters: weights and biases
-def save_params(W1, b1, W2, b2, W3, b3):
+def save_model_params(W1, b1, W2, b2, W3, b3):
     params = (W1, b1, W2, b2, W3, b3)
     params_file = open(r"C:\Users\smsou\GitHub Repos\Neural-Network-from-Scratch\model\params.pkl","wb")
     pickle.dump(params, params_file)
