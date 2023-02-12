36397c60ea10ac6c1305592fd74c7a0c64b359bb
Souvik Majumder <sm.souvikmajumder@gmail.com>
2023-02-12 19:12:49 +0530

    modified:   src/train.py

--- src/train.py
+++ src/train.py
@@ -1,3 +1,8 @@
+import sys
+import yaml
+from yaml import SafeLoader
+from pathlib import Path
+from constants import Constants
 from utils.dataset import *
 from utils.network import *
 from utils.prediction import *
@@ -5,17 +10,6 @@ from utils.save_model import *
 
 import warnings
 warnings.filterwarnings("ignore")
-
-################################# config variables #################################
-train_set = 'train'
-model_path = r"C:\Users\smsou\OneDrive\MY-DEVICE\Sync\Documents\GitHub\Neural-Network-from-Scratch\model\model.pkl" # add model_dir and model_filename
-units_layer_h1 = 10
-units_layer_h2 = 20
-units_layer_output = 10
-learning_rate = 0.10
-iterations = 100
-intervals = 10
-####################################################################################
 
 # fit the neural network with the train set
 # optimzer: gradient descent
@@ -43,8 +37,33 @@ def gradient_descent(X, y, alpha, iterations):
 
 if __name__ == '__main__':
 
+    # get the desired parent directory as root path
+    ROOT = Path(__file__).resolve().parents[1]
+
+    # add ROOT to sys.path if not present
+    if str(ROOT) not in sys.path:
+        # add ROOT to sys.path
+        sys.path.append(str(ROOT))
+
+    # load the config and parse it into a dictionary
+    with open(ROOT / Constants.CONFIG_PATH.value) as f:
+        slice_config = yaml.load(f, Loader = SafeLoader)
+
+    # get the model path from config
+    model_path = ROOT / slice_config['model']['model_dir'] / slice_config['model']['model_name']
+    # convert the path to a string in a format compliant with the current OS
+    model_path = model_path.as_posix()
+
+    # get the required variable values from config
+    units_layer_h1 = slice_config['model']['units_layer_h1']
+    units_layer_h2 = slice_config['model']['units_layer_h2']
+    units_layer_output = slice_config['model']['units_layer_output']
+    learning_rate = slice_config['optimizer']['learning_rate']
+    iterations = slice_config['train']['iterations']
+    intervals = slice_config['train']['intervals']
+
     # load and preprocess the train set
-    X_train, y_train = load_data(train_set)
+    X_train, y_train = load_data(Constants.TRAIN_SET.value)
     X_train = preprocess_data(X_train)
     # fit the defined neural network model with the training data and get the final weights and biases
     W1, b1, W2, b2, W3, b3 = gradient_descent(X_train, y_train, learning_rate, iterations)
