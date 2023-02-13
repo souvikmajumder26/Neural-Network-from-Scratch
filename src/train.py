88c90aaf953c0cca9c8b10ab92be7861963da500
Souvik Majumder <sm.souvikmajumder@gmail.com>
2023-02-13 23:31:52 +0530

    modified:   src/train.py

--- src/train.py
+++ src/train.py
@@ -2,14 +2,14 @@ import sys
 import yaml
 from yaml import SafeLoader
 from pathlib import Path
-from constants import Constants
+
 from utils.dataset import *
 from utils.network import *
 from utils.prediction import *
 from utils.save_model import *
 
-import warnings
-warnings.filterwarnings("ignore")
+from constants import Constants
+from logger import custom_logger
 
 # fit the neural network with the train set
 # optimzer: gradient descent
@@ -27,11 +27,11 @@ def gradient_descent(X, y, alpha, iterations):
         W1, b1, W2, b2, W3, b3 = update_params(W1, b1, W2, b2, W3, b3, dW1, db1, dW2, db2, dW3, db3, alpha)
         # display prediction details every 10th iteration
         if i == 0 or (i+1) % intervals == 0:
-            print("Iteration: {}".format(i + 1))
+            logger.info("Iteration: {}".format(i + 1))
             predictions = get_predictions(A3)
-            print("Predicted Labels: {}, True Labels: {}".format(predictions, y))
+            logger.info("Predicted Labels: {}, True Labels: {}".format(predictions, y))
             train_accuracy = get_accuracy(predictions, y) * 100
-            print("Train Accuracy: {:.2f}%".format(train_accuracy))
+            logger.info("Train Accuracy: {:.2f}%".format(train_accuracy))
     # return the final weights and biases
     return W1, b1, W2, b2, W3, b3
 
@@ -49,10 +49,23 @@ if __name__ == '__main__':
     with open(ROOT / Constants.CONFIG_PATH.value) as f:
         slice_config = yaml.load(f, Loader = SafeLoader)
 
-    # get the model path from config
-    model_path = ROOT / slice_config['model']['model_dir'] / slice_config['model']['model_name']
-    # convert the path to a string in a format compliant with the current OS
+    # get the model file dir from config
+    model_dir = ROOT / slice_config['model']['model_dir']
+    # make the directory if it does not exist
+    model_dir.mkdir(parents = True, exist_ok = True)
+    # get the model file path
+    model_path = model_dir / slice_config['model']['model_name']
+    # convert the path to string in a format compliant with the current OS
     model_path = model_path.as_posix()
+
+    # get the log file dir from config
+    log_dir = ROOT / slice_config['log']['log_dir']
+    # make the directory if it does not exist
+    log_dir.mkdir(parents = True, exist_ok = True)
+    # get the log file path
+    log_path = log_dir / slice_config['log']['log_name']
+    # convert the path to string in a format compliant with the current OS
+    log_path = log_path.as_posix()
 
     # get the required variable values from config
     units_layer_h1 = slice_config['model']['units_layer_h1']
@@ -61,6 +74,9 @@ if __name__ == '__main__':
     learning_rate = slice_config['optimizer']['learning_rate']
     iterations = slice_config['train']['iterations']
     intervals = slice_config['train']['intervals']
+    log_level = slice_config['log']['log_level']
+
+    logger = custom_logger("Neural Network Logs", log_path, log_level)
 
     # load and preprocess the train set
     X_train, y_train = load_data(Constants.TRAIN_SET.value)
@@ -69,4 +85,4 @@ if __name__ == '__main__':
     W1, b1, W2, b2, W3, b3 = gradient_descent(X_train, y_train, learning_rate, iterations)
     # save the trained model parameters: weights and biases
     save_model_params(W1, b1, W2, b2, W3, b3, model_path)
-    print("Model parameters saved!")
+    logger.info("Model parameters saved!")
